fa1065e4b3cee0a866950f8c474d31b637daab7f
Sergey Abrosov <sgabrosov@icloud.com>
2024-04-16 17:00:54 +0300

    Changed parameters for Docker-compose

--- Back-Sklif/Sources/App/configure.swift
+++ Back-Sklif/Sources/App/configure.swift
@@ -11,9 +11,9 @@ public func configure(_ app: Application) async throws {
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        username: Environment.get("DATABASE_USERNAME") ?? "rest_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "rest_password",
+        database: Environment.get("DATABASE_NAME") ?? "rest_database",
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
