9312f027b15333c83375fde85051b03ff67d8a2e
Sergey Abrosov <sgabrosov@icloud.com>
2024-05-16 15:28:57 +0300

    Added User Authorization

--- Back-Sklif/Sources/App/Controllers/AuthController.swift
+++ Back-Sklif/Sources/App/Controllers/AuthController.swift
@@ -5,6 +5,7 @@
 //  Created by Sergey Abrosov on 01.05.2024.
 //
 
+import Foundation
 import Fluent
 import Vapor
 
@@ -13,10 +14,10 @@ struct AuthController: RouteCollection {
         let usersGroup = routes.grouped("users")
         usersGroup.post(use: createUser)
         usersGroup.get(use: getAllUsers)
+        usersGroup.post("authorization", use: authUser)
     }
     
     func createUser(_ req: Request) async throws -> User {
-        
         guard let user = try? req.content.decode(User.self) else {
             throw Abort(.custom(code: 500, reasonPhrase: "Ошибка декодирования данных req в модель User"))
         }
@@ -29,5 +30,29 @@ struct AuthController: RouteCollection {
         let users = try await User.query(on: req.db).all()
         
         return users
+    }
+    
+    func authUser(_ req: Request) async throws -> User {
+        
+        struct UserModel: Codable {
+            let login: String
+            let password: String
+        }
+        
+        guard let byteBuffer = req.body.data else {
+            throw Abort(.badRequest)
+        }
+        
+        let user = try JSONDecoder().decode(UserModel.self, from: byteBuffer)
+
+        let users = try await User.query(on: req.db).all()
+        
+        for userFromDB in users {
+            if userFromDB.login == user.login && userFromDB.password == user.password {
+                return userFromDB
+            }
+        }
+        
+        throw Abort(.badRequest)
     }
 }
